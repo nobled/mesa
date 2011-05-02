295624edb588b5633f1d932a08077aa0767011d8
nobled <nobled@dreamwidth.org>
2011-05-02 17:52:28 +0000

    mesa: add yet more context fields for GL_ARB_debug_output

--- src/mesa/main/mtypes.h
+++ src/mesa/main/mtypes.h
@@ -3194,6 +3194,13 @@ OTHER_ERROR_OUT_OF_MEMORY,
 OTHER_ERROR_COUNT
 } OtherError;
 
+struct gl_client_debug
+{
+   GLboolean defaults[3][2][6]; /* severity, source, type */
+   /* TODO: Add an object here that can track the state of an arbitrary
+      number of client-provided IDs. */
+};
+
 struct gl_debug_state
 {
    GLDEBUGPROCARB Callback;
@@ -3203,8 +3210,7 @@ struct gl_debug_state
    GLboolean WinsysErrors[WINSYS_ERROR_COUNT];
    GLboolean ShaderErrors[SHADER_ERROR_COUNT];
    GLboolean OtherErrors[OTHER_ERROR_COUNT];
-   /* TODO: Add an object here that tracks the state of client-provided IDs
-      in the APPLICATION and THIRD_PARTY namespaces. */
+   struct gl_client_debug ClientIDs;
    struct gl_debug_msg Log[_MESA_MAX_DEBUG_LOGGED_MESSAGES];
    GLint NumMessages;
    GLint NextMsg;
