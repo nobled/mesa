5fe358e8b103d25d6a44bb2390d2c187ae8a29ad
Dylan Noblesmith <nobled@dreamwidth.org>
2012-03-12 22:21:00 +0000

    mesa: extend get_texobj for multitexturing

--- src/mesa/main/texparam.c
+++ src/mesa/main/texparam.c
@@ -117,17 +117,16 @@ validate_texture_wrap_mode(struct gl_context * ctx, GLenum target, GLenum wrap)
  * Only the glGetTexLevelParameter() functions accept proxy targets.
  */
 static struct gl_texture_object *
-get_texobj(struct gl_context *ctx, GLenum target, GLboolean get)
+get_texobj(struct gl_context *ctx, GLuint unit, GLenum target, GLboolean get)
 {
    struct gl_texture_unit *texUnit;
-
-   if (ctx->Texture.CurrentUnit >= ctx->Const.MaxCombinedTextureImageUnits) {
+   if (unit >= ctx->Const.MaxCombinedTextureImageUnits) {
       _mesa_error(ctx, GL_INVALID_OPERATION,
-                  "gl%sTexParameter(current unit)", get ? "Get" : "");
+                  "gl%sTexParameter(texunit = %u)", get ? "Get" : "", unit);
       return NULL;
    }
 
-   texUnit = _mesa_get_current_tex_unit(ctx);
+   texUnit = &(ctx->Texture.Unit[unit]);
 
    switch (target) {
    case GL_TEXTURE_1D:
@@ -741,7 +740,7 @@ _mesa_TexParameterf(GLenum target, GLenum pname, GLfloat param)
    struct gl_texture_object *texObj;
    GET_CURRENT_CONTEXT(ctx);
 
-   texObj = get_texobj(ctx, target, GL_FALSE);
+   texObj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_FALSE);
    if (!texObj)
       return;
 
@@ -796,7 +795,7 @@ _mesa_TexParameterfv(GLenum target, GLenum pname, const GLfloat *params)
    struct gl_texture_object *texObj;
    GET_CURRENT_CONTEXT(ctx);
 
-   texObj = get_texobj(ctx, target, GL_FALSE);
+   texObj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_FALSE);
    if (!texObj)
       return;
 
@@ -867,7 +866,7 @@ _mesa_TexParameteri(GLenum target, GLenum pname, GLint param)
    struct gl_texture_object *texObj;
    GET_CURRENT_CONTEXT(ctx);
 
-   texObj = get_texobj(ctx, target, GL_FALSE);
+   texObj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_FALSE);
    if (!texObj)
       return;
 
@@ -910,7 +909,7 @@ _mesa_TexParameteriv(GLenum target, GLenum pname, const GLint *params)
    struct gl_texture_object *texObj;
    GET_CURRENT_CONTEXT(ctx);
 
-   texObj = get_texobj(ctx, target, GL_FALSE);
+   texObj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_FALSE);
    if (!texObj)
       return;
 
@@ -970,7 +969,7 @@ _mesa_TexParameterIiv(GLenum target, GLenum pname, const GLint *params)
    struct gl_texture_object *texObj;
    GET_CURRENT_CONTEXT(ctx);
 
-   texObj = get_texobj(ctx, target, GL_FALSE);
+   texObj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_FALSE);
    if (!texObj)
       return;
 
@@ -999,7 +998,7 @@ _mesa_TexParameterIuiv(GLenum target, GLenum pname, const GLuint *params)
    struct gl_texture_object *texObj;
    GET_CURRENT_CONTEXT(ctx);
 
-   texObj = get_texobj(ctx, target, GL_FALSE);
+   texObj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_FALSE);
    if (!texObj)
       return;
 
@@ -1402,7 +1401,7 @@ _mesa_GetTexParameterfv( GLenum target, GLenum pname, GLfloat *params )
    struct gl_texture_object *obj;
    GET_CURRENT_CONTEXT(ctx);
 
-   obj = get_texobj(ctx, target, GL_TRUE);
+   obj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_TRUE);
    if (!obj)
       return;
 
@@ -1596,7 +1595,7 @@ _mesa_GetTexParameteriv( GLenum target, GLenum pname, GLint *params )
    struct gl_texture_object *obj;
    GET_CURRENT_CONTEXT(ctx);
 
-   obj = get_texobj(ctx, target, GL_TRUE);
+   obj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_TRUE);
    if (!obj)
       return;
 
@@ -1778,7 +1777,7 @@ _mesa_GetTexParameterIiv(GLenum target, GLenum pname, GLint *params)
    struct gl_texture_object *texObj;
    GET_CURRENT_CONTEXT(ctx);
 
-   texObj = get_texobj(ctx, target, GL_TRUE);
+   texObj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_TRUE);
    if (!texObj)
       return;
    
@@ -1799,7 +1798,7 @@ _mesa_GetTexParameterIuiv(GLenum target, GLenum pname, GLuint *params)
    struct gl_texture_object *texObj;
    GET_CURRENT_CONTEXT(ctx);
 
-   texObj = get_texobj(ctx, target, GL_TRUE);
+   texObj = get_texobj(ctx, ctx->Texture.CurrentUnit, target, GL_TRUE);
    if (!texObj)
       return;
    
